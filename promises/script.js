f019a2fe0b15a09b2843a83e842ad784cc43dd16
akranish1 <akranish0@gmail.com>
2025-06-26 00:39:32 +0530

    promise chaining

--- promises/script.js
+++ promises/script.js
@@ -1,2 +1,25 @@
 //Synchronous and Asynchronous 
 //want to add new line1
+function getData(data)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data",data);
+            resolve("Success");
+        },2000);
+    });
+}
+
+
+
+
+getData(1).then((res)=>
+{
+    return getData(2);
+}).then((res)=>
+{
+    return getData(3);
+}).then((res)=>
+{
+    console.log(res);
+});
